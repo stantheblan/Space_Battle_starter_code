5a1afbe0238cd4fb7530daceb014e5f8ba04e20c
Stan <stantonmoore42@gmail.com>
2022-02-11 15:44:39 -0500

    updated app.js
    final MVP before comments

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 class Ship {
-    constructor (h, f, acc)
-    {
+    constructor (h, f, acc) {
         this.hull = h;
         this.firepower = f;
         this.accuracy = acc;
@@ -9,34 +8,28 @@ class Ship {
 }
 
 class USSSchwarzenegger extends Ship {
-    constructor(h, f, acc)
-    {
+    constructor(h, f, acc) {
         super(h, f, acc)
         this.hull = h;
         this.firepower = f;
         this.accuracy = acc;
     }    
-    attack(opp)
-    {
+    attack(opp) {
         console.log(opp)
         let rdmN = Math.random();
 
-        if (this.accuracy > rdmN) 
-        {       
+        if (this.accuracy > rdmN) {       
             console.log("I hit " + this.firepower + " on their " + opp.hull + " hull")
 
             opp.hull -= this.firepower;
 
-            if (opp.hull <= 0)
-            {
+            if (opp.hull <= 0) {
                 opp.alive = false;
-                boolFleet[getIndex()] = false;
                 console.log("Alien has died.");
             }
         } 
-        else 
-        {
-            console.log(`I missed!`)
+        else {
+            console.log("I missed.");
         }
         console.log("Alien has " + opp.hull + " hull left.");
     }
@@ -68,34 +61,26 @@ class Enemy extends Ship {
         } 
         else 
         {
-            console.log(`They missed!`)
+            console.log("They missed!")
         }
         console.log("You have " + opp.hull + " hull left.");
     }
 }
 
-function battle(p, eA)
-{
+function battle(p, eA) {
     let cont = window.prompt("Attack y/n", "y");
     if (cont == null || cont.toLowerCase() == "n") {
-        console.log("u scardi cat")
-    }
-    else if (cont.toLowerCase() == "y")
-    {
-        for(let i = 0; i < eA.length; i++)
-        {
-            setIndex(i)
-            if (p.alive)
-            {
+        console.log("u scardi cat")}
+    else if (cont.toLowerCase() == "y") {
+        for(let i = 0; i < eA.length; i++) {
+            if (p.alive) {
                 let ans = window.prompt("Do you want to retreat? You have " + p.hull + " against their " + eA[i].hull, "n");
-                if (ans.toLowerCase() == "n")
-                {
+                if (ans.toLowerCase() == "n" || !p.alive)  {
                     console.log("\n\nBattle " + (i + 1))
                     while (eA[i].alive)
                     {  
                         p.attack(eA[i]);
-                        if (eA[i].alive == true)
-                        {
+                        if (eA[i].alive == true) {
                             eA[i].attack(p);
                             if (!p.alive) {break;}
                         }
@@ -105,6 +90,14 @@ function battle(p, eA)
             } 
             updateHTMLp(p)
             updateHTMLe(eA[i])
+            if (i+1 == eA.length && p.alive) {
+                console.log("WINNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN")
+                alert("WINNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN")
+            }
+        }
+        if (!p.alive) {
+            console.log("LOSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSER")
+            alert("LOSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSER")
         }
     }
     else {
@@ -113,62 +106,36 @@ function battle(p, eA)
     }
 }
 
-function makeFleet(num)
-{
-    for (let i = 0; i < num; i++)
-    {
+function makeFleet(num) {
+    for (let i = 0; i < num; i++) {
         enFleet[i] = new Enemy();
-        boolFleet[i] = true;
     }
 }
 
-function updateHTMLp(opp)
-{
+function updateHTMLp(opp) {
     document.getElementById("pStat").innerHTML = 
     "Hull: " + opp.hull  + "<br>"
     + "FirePower: " + opp.firepower + "<br>"
     + "Accuracy: " + opp.accuracy;
 }
 
-function updateHTMLe(opp)
-{
+function updateHTMLe(opp) {
     document.getElementById("eStat").innerHTML = 
     "Hull: " + opp.hull  + "<br>"
     + "FirePower: " + opp.firepower + "<br>"
     + "Accuracy: " + opp.accuracy;
 }
 
-let checker = arr => arr.every(v => v === false)
-
-function getIndex()
-{
-    return indexA;
-}
-
-function setIndex(i)
-{
-    indexA = i;
-}
-
 let schw = new USSSchwarzenegger(20, 5, 0.7);
+
 let enFleet = [];
-let boolFleet = [];
+
 let indexA = 0;
 let rdmF = Math.random() * 50;
-makeFleet(5);
 
 
-// console.log(schw)
-// console.log(en)
-
-// schw.attack(en)
-// en.attack(schw)
-
-// console.log(schw)
-// console.log(en)
+makeFleet(10);
 
 console.log(enFleet)
-// enFleet.every(checkPWin)
-console.log(checker(boolFleet))
+
 battle(schw, enFleet);
-console.log(checker(boolFleet))
